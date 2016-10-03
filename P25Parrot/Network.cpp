47cce73dfb869656580ffd5d4a74cb6343f91e18
Jonathan Naylor <naylorjs@yahoo.com>
2016-10-03 21:57:14 +0100

    Initial commit.

---
+++ P25Parrot/Network.cpp
@@ -0,0 +1,108 @@
+/*
+ *   Copyright (C) 2009-2014,2016 by Jonathan Naylor G4KLX
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "Network.h"
+#include "Utils.h"
+
+#include <cstdio>
+#include <cassert>
+#include <cstring>
+
+const unsigned int BUFFER_LENGTH = 200U;
+
+CNetwork::CNetwork(unsigned int port, bool debug) :
+m_socket(port),
+m_address(),
+m_port(0U),
+m_debug(debug),
+m_buffer(1000U, "P25 Network")
+{
+}
+
+CNetwork::~CNetwork()
+{
+}
+
+bool CNetwork::open()
+{
+	::fprintf(stdout, "Opening P25 network connection\n");
+
+	return m_socket.open();
+}
+
+bool CNetwork::write(const unsigned char* data, unsigned int length)
+{
+	if (m_port == 0U)
+		return true;
+
+	assert(data != NULL);
+
+	if (m_debug)
+		CUtils::dump(1U, "P25 Network Data Sent", data, length);
+
+	return m_socket.write(data, length, m_address, m_port);
+}
+
+void CNetwork::clock(unsigned int ms)
+{
+	unsigned char buffer[BUFFER_LENGTH];
+
+	in_addr address;
+	unsigned int port;
+	int length = m_socket.read(buffer, BUFFER_LENGTH, address, port);
+	if (length <= 0)
+		return;
+
+	m_address.s_addr = address.s_addr;
+	m_port = port;
+
+	if (m_debug)
+		CUtils::dump(1U, "P25 Network Data Received", buffer, length);
+
+	unsigned char l = length;
+	m_buffer.addData(&l, 1U);
+
+	m_buffer.addData(buffer, length);
+}
+
+unsigned int CNetwork::read(unsigned char* data)
+{
+	assert(data != NULL);
+
+	if (m_buffer.isEmpty())
+		return 0U;
+
+	unsigned char len = 0U;
+	m_buffer.getData(&len, 1U);
+
+	m_buffer.getData(data, len);
+
+	return len;
+}
+
+void CNetwork::end()
+{
+	m_port = 0U;
+}
+
+void CNetwork::close()
+{
+	m_socket.close();
+
+	::fprintf(stdout, "Closing P25 network connection\n");
+}
---
+++ P25Parrot/Parrot.cpp
@@ -0,0 +1,81 @@
+/*
+*   Copyright (C) 2016 by Jonathan Naylor G4KLX
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "Parrot.h"
+
+#include <cstdio>
+#include <cassert>
+#include <cstring>
+
+CParrot::CParrot(unsigned int timeout) :
+m_data(NULL),
+m_length(timeout * 1550U + 1000U),
+m_used(0U),
+m_ptr(0U)
+{
+	assert(timeout > 0U);
+
+	m_data = new unsigned char[m_length];
+}
+
+CParrot::~CParrot()
+{
+	delete[] m_data;
+}
+
+bool CParrot::write(const unsigned char* data, unsigned int length)
+{
+	assert(data != NULL);
+
+	if ((m_length - m_used) < (length + 2U))
+		return false;
+
+	m_data[m_used] = length;
+	::memcpy(m_data + m_used + 1U, data, length);
+	m_used += length + 1U;
+
+	return true;
+}
+
+void CParrot::end()
+{
+	m_ptr = 0U;
+}
+
+void CParrot::clear()
+{
+	m_used = 0U;
+	m_ptr = 0U;
+}
+
+unsigned int CParrot::read(unsigned char* data)
+{
+	assert(data != NULL);
+
+	if (m_used == 0U)
+		return 0U;
+
+	unsigned int length = m_data[m_ptr];
+	::memcpy(data, m_data + m_ptr + 1U, length);
+	m_ptr += length + 1U;
+
+	if (m_ptr >= m_used)
+		m_used = 0U;
+
+	return length;
+}
