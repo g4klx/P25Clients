9a791e85a8562b3baf8f7b78bc999995c88c759c
Chipster <go@w.ay>
2025-07-09 16:27:40 -0500

    Update json hostfile downloader for RefCheck.Radio hosts

--- P25Gateway/P25HostsUpdate.sh
+++ P25Gateway/P25HostsUpdate.sh
@@ -38,6 +38,6 @@ then
 fi
 
 # Download P25Hosts.json file
-curl -X 'GET' 'https://dvref.com/p25/reflectors/' -H 'accept: */*' > ${P25HOSTS}
+curl --fail --silent -S -L -o  ${NXDNHOSTS} -A "NXDNGateway - G4KLX" https://hostfiles.refcheck.radio/P25Hosts.txt
 
 exit 0
